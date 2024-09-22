2f2161807e828299a82e83da82306d4d89b1eb60
Jared <jaredmgray@outlook.com>
2024-09-22 07:25:05 -0500

    updated js with game logic and to update score after points

--- javascript.js
+++ javascript.js
@@ -1,7 +1,24 @@
 const buttons = document.querySelectorAll(".choices button")
 const gameplay = document.querySelector("#gameplay")
+const compScoreBoard = document.querySelector(".computerscore")
+const humanScoreBoard = document.querySelector(".humanscore")
 
-const rock = ''
+let humanChoice;
+let computerChoice;
+
+let computerScore = 0
+let humanScore = 0
+
+const computerPoints = document.createElement("div")
+computerPoints.classList.add("choice")
+computerPoints.textContent = computerScore
+
+const humanPoints = document.createElement("div");
+humanPoints.classList.add("points")
+humanPoints.textContent = humanScore
+
+humanScoreBoard.appendChild(humanPoints)
+compScoreBoard.appendChild(computerPoints)
 
 function getComputerChoice(){
     //Picks a random number 0-2
@@ -16,12 +33,30 @@ function getComputerChoice(){
     }
 }
 
+function gameLogic (humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        return "tie"
+    }else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+    }else {
+        computerScore++;
+    }
+    humanPoints.textContent = humanScore
+    computerPoints.textContent = computerScore
+}
+
 buttons.forEach((button) => {
     button.addEventListener('click', () =>{
         gameplay.innerHTML = ""
 
-        const humanChoice = button.value;
-        const computerChoice = getComputerChoice()
+        humanChoice = button.value;
+        computerChoice = getComputerChoice()
+
+        gameLogic(humanChoice, computerChoice)
 
         if (button.value === "rock"){
             const choice = document.createElement("img")
@@ -42,7 +77,6 @@ buttons.forEach((button) => {
             gameplay.appendChild(choice)
         }
       
-        
         if (computerChoice === "rock"){
             const compPick = document.createElement("img")
             compPick.classList.add("computerChoice")
@@ -54,82 +88,12 @@ buttons.forEach((button) => {
             compPick.classList.add("computerChoice")
             compPick.src = "./pictures/paper.png"
             gameplay.appendChild(compPick)
-            
+
         }else if (computerChoice === "scissors"){
             const compPick = document.createElement("img")
             compPick.classList.add("computerChoice")
             compPick.src = "./pictures/scissors.png"
             gameplay.appendChild(compPick)
         }
-        
-        
-        
-
-        
     })
 })
-// //base score of the game
-// let computerScore = 0
-// let humanScore = 0
-// //logic behind rock paper scissors
-// function playRound(humanChoice, computerChoice){
-
-        
-//     if (humanChoice == computerChoice){
-
-//     }else if (humanChoice === "rock" && computerChoice === "paper"){
-//         computerScore++;
-
-//     }else if (humanChoice === "rock" && computerChoice === "scissors"){
-//         humanScore++;
-
-//     }else if (humanChoice === "paper" && computerChoice === "scissors"){
-//         computerScore++;
-
-//     }else if (humanChoice === "paper" && computerChoice === "rock"){
-//         humanScore++;
-
-//     }else if (humanChoice === "scissors" && computerChoice === "rock"){
-//         computerScore++;
-
-//     }else {
-//         humanScore++;
-//     }
-// }
-// //loop to make the game 5 rounds long
-// for (let round = 1; round <= 5; round ++){
-//     //calling these and assigning them to new variables
-//     const humanSelection = getHumanChoice()
-//     const computerSelection = getComputerChoice()
-//     //if tie, allows for an additional round
-//     if (humanSelection === computerSelection){
-//         console.log(`%cIt Was A Tie, Try Again`,
-//             `color: pink; font-weight: 1000`
-//         )
-//         round--;
-//     }
-//     playRound(humanSelection, computerSelection)
-//     //keeping score visable for user
-//     console.log(`%cRound ${round}`,
-//         `color: gray; font-size: 16px; font-weight: 1000`
-//     )
-//     console.log(`Your Score %c${humanScore}`,
-//         `color: lime; font-size: 16px; font-weight: 1000`
-//     )
-//     console.log(`Computer's Score %c${computerScore}`,
-//         `color: red; font-size: 16px; font-weight: 1000`
-//     )
-// }
-// //winning system`~
-// if (humanScore > computerScore){
-//     console.log(`%cYou Won The Game By A ${humanScore - computerScore} Point Difference` + `  :)`,
-//         `color: lime; font-size: 20px; font-weight: 1000`
-//     )
-// }else if (humanScore < computerScore){
-//     console.log(`%cYou Lost The Game And The Computer Won By A ${computerScore - humanScore} Point Difference` + `  :(`,
-
-//         `color: red; font-size: 20px; font-weight: 1000`
-//     )
-// }else {
-//     console.log("It's a Tie!")
-// }
